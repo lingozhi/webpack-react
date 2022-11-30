17097704e001befc1a62c15ba20e3da35f074e3b
chen.lingzhi <chen.lingzhi@qkupower.com>
2022-11-30 17:29:41 +0800

    编写eslint、prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/jsx-runtime",
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        "@typescript-eslint/no-var-requires": 0,
+        "react/jsx-uses-react": "off", // 关闭旧模式校验
+        "react/react-in-jsx-scope": "off", // 关闭旧模式校验
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};
