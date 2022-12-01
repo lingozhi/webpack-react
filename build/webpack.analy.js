79791c9ed3a465cd8db4b3f1ce0d5b23947203bc
chen.lingzhi <chen.lingzhi@qkupower.com>
2022-12-01 10:14:03 +0800

    feat: 添加样式格式化

--- build/webpack.analy.js
+++ build/webpack.analy.js
@@ -5,9 +5,9 @@ const smp = new SpeedMeasurePlugin();
 const { merge } = require("webpack-merge");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer"); // 引入分析打包结果插件
 module.exports = smp.wrap(
-  merge(prodConfig, {
-    plugins: [
-      new BundleAnalyzerPlugin(), // 配置分析打包结果插件
-    ],
-  })
+    merge(prodConfig, {
+        plugins: [
+            new BundleAnalyzerPlugin(), // 配置分析打包结果插件
+        ],
+    })
 );
